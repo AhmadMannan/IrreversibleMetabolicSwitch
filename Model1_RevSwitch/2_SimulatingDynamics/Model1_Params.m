2709730744bc1cab0cf33b5c8567fefce8d4cec2
Ahmad Mannan <mannan.ahmad@yahoo.co.uk>
2021-05-06 15:24:24 +0100

    Create Model1_Params.m

---
+++ Model1_RevSwitch/2_SimulatingDynamics/Model1_Params.m
@@ -0,0 +1,76 @@
+% ------------------------------------------------------------------------
+% DEFINING PARAMETERS OF MODEL 1
+% ------------------------------------------------------------------------
+% Ahmad A. Mannan
+% 12/11/2020
+% ------------------------------------------------------------------------
+
+function params = Model1_Params(arch)
+
+
+% Load optimally fitted parameter values:
+load('Params_OptFit_NAR.mat') %#ok<*LOAD>
+p = Params_OptFit_NAR;
+
+% FadR expression and regulation:
+  b_R   = p(2);  % Basal leaky expression of FadR.
+  a_R   = p(3);  % Maximum rate of expression of FadR.
+  K_R   = p(4);  % Affinity (ratio of rate of binding to unbinding) of FadR to its promoter.
+
+% FadD expression and regulation:
+  b_D   = p(6);  % Basal leaky expression of FadD.
+  a_D   = p(7);  % Maximum rate of expression of FadD.
+  K_D   = p(8);  % Affinity (ratio of rate of binding to unbinding) of FadR to FadD promoter.
+
+% Uptake Reaction (single pseudo-rxn representing FadL and FadD):
+  kcatD = p(10); % Turnover rate of enzyme of FadD.
+  Km_D  = p(11); % Michaelis-constant of enzyme of FadD for Fatty Acid (OA).
+  
+% PlsB enzyme kinetics - Glycerol-3-Phosphate Acyltransferase:
+  kcatB = p(12); % Turnover rate of enzyme of PlsB.
+  Km_B  = p(13); % Michaelis-constant of enzyme of PlsB for Acyl-CoA.
+  B     = p(14); % Assumed constant levels of PlsB enzyme.
+
+% Sequestration of FadR by Acyl-CoA:
+  kf    = p(15); % Effective forward rate of FadR sequestration by Acyl-CoA.
+  kr    = p(16); % Effective reverse rate of sequestering.
+
+% Eg expression and regulation:
+  a_g   = p(1); % Maximum synthesis rate of enzyme Eg.
+  K_g   = 4.9114; % Affinity of FadR to activate Eg synthesis, based on FadR PAR promoter.
+  
+% Growth rate:
+  muMax = p(1); % Assumed constant, as seen in experiments of Di Liu.
+  sT    = 0.7; % Minimum growth rate when no Ep expressed, based on Usui 2012 report that mu is reduced to 30% of its value when pgi is knocked out (Usui 2013), and growth in our cell is at 0.1818 1/h.
+
+% Affinity of FadR to inhibit Ep synthesis.
+  K_p   = p(8);
+
+% Defining the vector of parameters:
+  p2 = [b_R;
+        a_R;
+        K_R;
+        b_D;
+        a_D;
+        K_D;
+        kcatD;
+        Km_D;
+        kcatB;
+        Km_B;
+        B;
+        kf;
+        kr;
+        a_g;
+        K_g;
+        muMax;
+        sT;
+        K_p];
+
+% Redefine parameters for circuit topologies with NAR and PAR, so that all
+% three synthesis same steady state conc of FadR in absence of inducer:
+    params = p2;
+if strcmp(arch,'NAR') == 1
+    % do nothing
+elseif strcmp(arch,'PAR') == 1
+    params(3) = p2(3) * 7;
+end
